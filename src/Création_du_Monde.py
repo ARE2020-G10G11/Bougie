628329b8c6ab7509e509cbe2ff11fb2acbe213d8
aya <61982800+aya-ikezawa@users.noreply.github.com>
2020-04-06 17:10:11 +0200

    Update and rename python to Création_du_Monde.py

---
+++ src/Création_du_Monde.py
@@ -0,0 +1,124 @@
+from math import *
+
+def monde(d,n,p):
+    #(distance - la distance entre les deux bougies)
+    #(l'intensité = nombre de bougies collées. n entre 1 et 4.)
+    
+    
+    #monde : list[list[int]]
+    monde = []
+    #c : int
+    c = 0
+    #e : int
+    e = 0
+    #f : int
+    f = n-1
+            
+    
+    for i in range(1,25):
+        # list : list[int]
+        list = []
+        
+        if p == 0:
+            
+            if i >= 24-(3*n-2):
+                for j in range(1,24):
+                    if j <=(12-int(d/2))+c and j>=(12-int(d/2))-c:
+                        list.append(1)
+                    elif j <=(13+int(d/2))+c and j>=(13+int(d/2))-c:
+                        list.append(1)
+                    else:
+                        list.append(0)
+               
+                if c < n-1:     
+                    c = c+1        
+            else:
+                for j in range(1,25):
+                    list.append(0)
+                            
+            monde.append(list) 
+            
+        elif p == 90 or p == 270:
+            
+            if i < 24-((3*n)-2+n):
+                for j in range(1,25):
+                    list.append(0)
+                monde.append(list)
+                
+            elif i < 24-(3*n-2):
+                for j in range(1,25):
+                    if j <=(13+int(d/2))+e and j>=(13+int(d/2))-e:
+                        list.append(1)
+                    else:
+                        list.append(0)
+                if e < n-1:     
+                    e = e+1 
+                monde.append(list)
+                
+            else:
+                for j in range(1,25):
+                    if j <=(12-int(d/2))+c and j>=(12-int(d/2))-c:
+                        list.append(1)
+                    elif j <=(13+int(d/2))+e and j>=(13+int(d/2))-e:
+                        list.append(1)
+                    else:
+                        list.append(0)
+                
+                if c < n-1:     
+                    c = c+1
+                if e < n-1:     
+                    e = e+1 
+                            
+                monde.append(list)
+        
+        elif p == 180:
+            
+            if i < 24-((6*n)-4):
+                for j in range(1,25):
+                    list.append(0)
+                monde.append(list)
+                
+            elif i < 24-(4*n-2):
+                for j in range(1,25):
+                    if j <=(13+int(d/2))+f and j>=(13+int(d/2))-f:
+                        list.append(1)
+                    else:
+                        list.append(0)
+                monde.append(list)
+                
+            elif i < 24-(3*n-2):
+                for j in range(1,25):
+                    if j <=(13+int(d/2))+f and j>=(13+int(d/2))-f:
+                        list.append(1)
+                    else:
+                        list.append(0)
+                if f > 0:
+                    f = f-1
+                monde.append(list)
+                      
+
+            else:
+                for j in range(1,25):
+                    if j <=(12-int(d/2))+c and j>=(12-int(d/2))-c:
+                        list.append(1)
+                    elif j <=(13+int(d/2))+e and j>=(13+int(d/2))-e:
+                        list.append(1)
+                    else:
+                        list.append(0)
+                
+                if c < n-1:     
+                    c = c+1
+                if e < n-1:     
+                    e = e+1 
+                            
+                monde.append(list)
+        
+                
+            
+       
+                    
+    return monde
+
+
+            
+                     
